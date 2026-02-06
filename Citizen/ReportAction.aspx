7eee9c7733b0048089c356bd577c440a6a313fe2
Tejas Tholar <tejas.tholar@somaiya.edu>
2026-02-06 20:34:19 +0530

    ReportAction page updated

---
+++ Citizen/ReportAction.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true"
+    CodeBehind="ReportAction.aspx.cs"
+    Inherits="Traffic_Violation_Detection_System.Admin.ReportAction" %>
+
+<form runat="server">
+    <h2>Report Action</h2>
+
+    Vehicle Number:
+        <asp:Label ID="lblVehicle" runat="server"></asp:Label>
+        <br /><br />
+
+        Location:
+        <asp:Label ID="lblLocation" runat="server"></asp:Label>
+        <br /><br />
+        
+         Description:
+         <asp:Label ID="Descrip" runat="server"></asp:Label>
+         <br /><br />
+
+        Violation:
+        <asp:Label ID="lblViolation" runat="server"></asp:Label>
+        <br /><br />
+
+        Proof Image:<br />
+        <asp:Image ID="imgProof" runat="server" Width="300px" />
+        <br /><br />
+
+
+    Status:
+    <asp:DropDownList ID="ddlStatus" runat="server">
+        <asp:ListItem>Pending</asp:ListItem>
+        <asp:ListItem>Approved</asp:ListItem>
+        <asp:ListItem>Rejected</asp:ListItem>
+    </asp:DropDownList>
+
+    <br /><br />
+
+    <asp:Button ID="btnUpdate" runat="server"
+        Text="Update Status"
+        OnClick="btnUpdate_Click" />
+
+    <br /><br />
+    <asp:Label ID="lblMsg" runat="server"></asp:Label>
+</form>
---
+++ Citizen/ReportAction.aspx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Traffic_Violation_Detection_System.Admin
+{
+    public partial class ReportAction : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+                LoadReport();
+        }
+
+        void LoadReport()
+        {
+            string id = Request.QueryString["id"];
+            if (string.IsNullOrEmpty(id)) return;
+
+            SqlConnection con = new SqlConnection(
+                ConfigurationManager.ConnectionStrings["dbcon"].ConnectionString);
+
+            string q = "SELECT * FROM Reports WHERE ReportId=@id";
+            SqlCommand cmd = new SqlCommand(q, con);
+            cmd.Parameters.AddWithValue("@id", id);
+
+            con.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            if (dr.Read())
+            {
+                lblVehicle.Text = dr["VehicleNo"].ToString();
+                lblLocation.Text = dr["Location"].ToString();
+                Descrip.Text = dr["Description"].ToString();
+                lblViolation.Text = dr["ViolationType"].ToString();
+                imgProof.ImageUrl = "../Citizen/" + dr["ProofPath"].ToString();
+                
+            }
+
+            con.Close();
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string id = Request.QueryString["id"];
+
+            if (string.IsNullOrEmpty(id))
+            {
+                lblMsg.Text = "Report ID missing!";
+                return;
+            }
+
+            SqlConnection con = new SqlConnection(
+                ConfigurationManager.ConnectionStrings["dbcon"].ConnectionString);
+
+            string q = "UPDATE Reports SET Status=@s WHERE ReportId=@id";
+
+            SqlCommand cmd = new SqlCommand(q, con);
+            cmd.Parameters.AddWithValue("@s", ddlStatus.Text);
+            cmd.Parameters.AddWithValue("@id", id);
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            lblMsg.Text = "Status Updated!";
+        }
+
+    }
+}
+
